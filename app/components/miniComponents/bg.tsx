1b78ea03151e502b528867d0d0f26bef1a360089
Aalekh <dheetaalekh@gmail.com>
2024-11-10 07:56:55 +0530

    aalekh assignments

--- app/components/miniComponents/bg.tsx
+++ app/components/miniComponents/bg.tsx
@@ -1,16 +1,22 @@
+
+
 // import Image from "next/image";
 
-// const Bg = ({children}) => {
+// const Bg = ({ children }) => {
 //   return (
-//     <div className="relative w-full h-screen ">
+//     <div className="relative w-full min-h-screen">
 //       <Image
 //         src="/assets/bg.png"
 //         layout="fill"
 //         objectFit="cover"
 //         alt="Background Image"
-//         className="absolute inset-0 -z-10"
+//         className="absolute inset-0 -z-10 object-cover w-full h-full"
+//         sizes="(max-width: 640px) 100vw, (max-width: 768px) 100vw, (max-width: 1024px) 100vw, 100vw"
+//         priority // Loads the image as a priority for better performance
 //       />
-//       {children}
+//       <div className="relative flex flex-col items-center justify-center px-4 py-8 sm:px-6 lg:px-8 xl:px-10 text-center">
+//         {children}
+//       </div>
 //     </div>
 //   );
 // };
@@ -18,11 +24,15 @@
 // export default Bg;
 
 
-// responsive
 
 import Image from "next/image";
+import { ReactNode } from "react";
 
-const Bg = ({ children }) => {
+interface BgProps {
+  children: ReactNode;
+}
+
+const Bg: React.FC<BgProps> = ({ children }) => {
   return (
     <div className="relative w-full min-h-screen">
       <Image
